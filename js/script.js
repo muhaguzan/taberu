2803a638f4923148e3660dbfea52ccfe28b4de17
muhaguzan <mgustifauzan2794@gmail.com>
2024-01-23 20:41:48 +0700

    Taberu

---
+++ js/script.js
@@ -0,0 +1,22 @@
+// navbarfixed
+window.onscroll = function() {
+    const header = document.querySelector('header');
+    const fixedNav = header.offsetTop;
+
+    if(window.pageYOffset > fixedNav) {
+        header.classList.add('navbar-fixed');
+    } else {
+        header.classList.remove('navbar-fixed');
+    }
+}
+
+
+
+// hamburger
+const hamburger = document.querySelector('#hamburger');
+const navMenu = document.querySelector('#navmenu');
+
+hamburger.addEventListener('click', function(){
+    hamburger.classList.toggle('hamburger-active');
+    navMenu.classList.toggle('hidden');
+});
